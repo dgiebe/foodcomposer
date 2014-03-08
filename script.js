fb5daa49e200eb5f3c023d9ef90cbc309961d040
broszio <fabian@web-exclusivity.com>
2014-03-08 17:50:26 +0100

    Stable 1.1
    - Swipeleft Funktion zum vergrößern des Elements
    - jquery mobile lib
    - hammer.js lib

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 $(function() {
+	
+	
     $( ".toppings" ).draggable();
-
-    $( "#pizza" ).droppable({
+    $( ".pizza" ).droppable({
       drop: function( event, ui ) {
 		  
 		  inc = $(ui.draggable).attr("id");
@@ -12,6 +13,26 @@ $(function() {
           .find( "p" )
             .html( "Dropped!" );
 			$(ui.draggable).fadeOut("300");
-      }
+	  }
+	  
     });
+
+	
+	$( ".swippiz" ).on( "swipeleft", pizza34);
+
+	$( ".swippiz" ).on( "swiperight", pizza38);
+
+
+	
+	function pizza38() {
+		$('.pizza').addClass('pizza38')
+		$('.swippiz').addClass('swippiz38')
+		console.log('pizza38 is happening')
+		};
+	function pizza34() {
+		$('.pizza38').removeClass('pizza38')
+		$('.swippiz38').removeClass('swippiz38')
+		console.log('pizza34 is happening')
+		};
+		
   });
